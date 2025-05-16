623d196eaaebd73b89769cb851b553f63c01e2f3
Akash Welgama <139337308+Akashwelgama@users.noreply.github.com>
2025-05-16 08:59:44 +0530

    feat: Hesnid spring security starting

---
+++ Spring/ProjectFive_secureDemo/src/test/java/com/cruiser/secureDemo/SecureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cruiser.secureDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
